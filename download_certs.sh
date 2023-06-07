26ea44acdda5a41e992afaad5d5bf337c7d0282b
abessiari <aessiari@lbl.gov>
2023-06-06 17:16:38 -0700

    Ssl support (#1)
    * ssl-support
    
    * qos/ssl-support
    
    * using aws cli

---
+++ download_certs.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+echo `date`
+source /download_certs_source.sh
+DESTINATION=/tmp/ssl-credentials.tar.gz
+TAR_DESTINATION=/tmp/ssl-certs
+
+mkdir -p /opt/credentials
+
+SLEEP_DURATION=5
+tries=3
+i=0
+
+while true 
+   do
+     echo  "Dowloading ssl credentials from $S3_SSL_CERTS_LOCATION Try: $i ..."
+     aws s3 cp $S3_SSL_CERTS_LOCATION $DESTINATION
+     ret=$?
+
+     if [ $ret != 0 ]; then
+        echo  "Error dowloading ssl credentials from S3_SSL_CERTS_LOCATION=$S3_SSL_CERTS_LOCATION"
+     else
+	break
+     fi
+
+     ((i++))
+
+     if [ $i -lt $tries ]; then
+        echo  "Going to sleep for $SLEEP_DURATION ..."
+        sleep $SLEEP_DURATION 
+     else
+        echo  "Giving up on downloading ssl credentials from $S3_SSL_CERTS_LOCATION ..."
+	exit 1
+     fi
+done
+
+rm -rf $TAR_DESTINATION
+mkdir -p $TAR_DESTINATION
+tar -xzf $DESTINATION -C $TAR_DESTINATION 
+cert_file=`find $TAR_DESTINATION -name fullchain.pem`
+key_file=`find $TAR_DESTINATION -name privkey.pem`
+
+if [ ! -f "$cert_file" ]; then
+   echo "Did not find cert file. Exiting ..."
+   rm -rf $TAR_DESTINATION
+   rm -rf $DESTINATION
+   exit 2 
+fi
+
+echo diff $cert_file /opt/credentials/fullchain.pem
+diff $cert_file /opt/credentials/fullchain.pem > /dev/null 2>&1
+ret=$?
+
+if [ $ret = 0 ]; then
+   echo  "Ssl credentials are sane. Doing nothing..."
+else
+   echo  "Copying ssl credentials ..."
+   cp $cert_file /opt/credentials
+   cp $key_file /opt/credentials
+   chmod 400 /opt/credentials/fullchain.pem
+   chmod 400 /opt/credentials/privkey.pem
+   echo  "Reloading apache service ..."
+   /usr/sbin/service apache2 reload > /dev/null 
+fi
+rm -rf $TAR_DESTINATION
+rm -rf $DESTINATION
+exit 0 
--- entrypoint-proxy.sh
+++ entrypoint-proxy.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
+USE_SSL="${USE_SSL:=1}"
+
+if [ $USE_SSL -ne 0 ]; then
+   if [ ! -f "/download_certs_source.sh " ]; then
+      echo S3_SSL_CERTS_LOCATION=$S3_SSL_CERTS_LOCATION > download_certs_source.sh 
+   fi
+
+   if [ -f "/crontabs.txt" ]; then
+      echo " Installing crontabs ..." 
+      crontab /crontabs.txt 
+      rm /crontabs.txt
+   fi
+   echo "Dowloading ssl credentials ..." 
+   /opt/bin/download_certs.sh >> /tmp/download_certs.log 2>&1
+fi
+
+echo " Restart cron ...." 
+service cron restart
+
 USE_S3="${USE_S3:=1}"
 
 if [ $USE_S3 -ne 0 ]; then
-   if [[ ! "$(service cron status)" =~ "start/running" ]]
-   then
-      echo " The cron service has been stopped. It has now been restarted." 
-      service cron start
-   else
-      echo " The cron service has been restarted." 
+   if [ -f "/apache2" ]; then
+       mv /apache2 /etc/logrotate.d/apache2
+       chmod 400 /etc/logrotate.d/apache2
    fi
 fi
 
-if [[ ! "$(service apache2 status)" =~ "start/running" ]]
-then
-    echo " The Apache service has been stopped. It has now been restarted." 
-    service apache2 start
-else
-    echo " The Apache service has been restarted." 
-fi
+echo " Restart apache service." 
+service apache2 restart
 
 exec "$@"
